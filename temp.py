ec03c1452bcedb01d93d39f571f448d31c16964d
dishitamishra10 <158179175+dishitamishra10@users.noreply.github.com>
2026-03-19 00:14:18 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,29 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, BaseMessage
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    model="Qwen/Qwen2.5-72B-Instruct",
+    task='text-generation'
+)
+
+model = ChatHuggingFace(llm=llm)
+
+chat_history : list[BaseMessage] = [
+    SystemMessage(content="You are a helpful AI assistant")
+]
+
+while True:
+    user_input = input("Enter : ")
+    if user_input == 'exit':
+        break
+
+    chat_history.append(HumanMessage(content=user_input))
+
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI : ", result.content)
+
+print(chat_history)
